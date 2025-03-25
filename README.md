ff7290ee057f1298dd4741d1aa49371e3329ec29
bleds1 <bleds1@users.noreply.github.com>
2025-03-25 17:21:35 +0000

    202403251721 script updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Scripts
 
-Various scripts some I use and some don't but keep around..never know when you might need 'em right.
+Various scripts
--- dwmbar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Statusbar loop for displaying time and date in dwm statusbar
-while true; do
-   xsetroot -name "$( date +"   %F %R" )"
-   sleep 1m    # Update time every minute
-done &
